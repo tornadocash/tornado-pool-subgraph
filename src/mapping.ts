a129d70e88afffff1be3b46464027a0608c1f4ba
Danil Kovtonyuk <danx.kov@gmail.com>
2021-10-15 16:11:20 +1000

    init

---
+++ src/mapping.ts
@@ -0,0 +1,14 @@
+import { Account } from "../generated/schema";
+import { PublicKey } from "../generated/BridgeHelper/BridgeHelper";
+
+export function handlePublicKey(event: PublicKey): void {
+  let entity = new Account(
+    event.transaction.from.toHex() + "-" + event.logIndex.toString()
+  );
+
+  entity.key = event.params.key;
+  entity.owner = event.params.owner;
+  entity.blockNumber = event.block.number;
+
+  entity.save();
+}
